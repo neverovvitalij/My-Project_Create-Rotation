3579d5a287286c45c09249bfe513e9cb9d8a935a
Vitalij Neverov <vitalij.neverov@gmail.com>
2025-04-18 10:41:50 +0200

    local changes

---
+++ frontend/src/http/index.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+export const REACT_APP_API_URL = 'http://localhost:8080/api';
+
+const api = axios.create({
+  withCredentials: true,
+  baseURL: REACT_APP_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get(`${REACT_APP_API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem('token', response.data.accessToken);
+        return api.request(originalRequest);
+      } catch (error) {
+        console.log(error, 'Not authorized');
+      }
+    }
+    throw error;
+  }
+);
+
+export default api;
