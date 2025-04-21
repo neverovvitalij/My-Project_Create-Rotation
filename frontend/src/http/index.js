f40bfb9580e9c09ba10b3f5e44c78ca5c1f9fdc8
Vitalij Neverov <vitalij.neverov@gmail.com>
2025-04-21 17:38:13 +0200

    index http frontend

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const REACT_APP_API_URL = 'http://localhost:8080/api';
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   withCredentials: true,
